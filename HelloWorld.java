eb566ae682b0800ac4951b750191dcd300127d72
sharathkashi <42238416+sharathkashi@users.noreply.github.com>
2021-07-06 15:20:30 +0530

    CLA added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String []args){
-    System.out.println("Hello....,Let's Code on JAVA");
+    System.out.println("Hello "+args[0]+"....,\nLet's Code on JAVA");
   }
 }
