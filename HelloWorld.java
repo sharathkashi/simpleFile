8b7c78599c94d3425b9af4e1cf096edd9b35655e
sharathkashi <42238416+sharathkashi@users.noreply.github.com>
2023-01-24 13:51:01 +0530

    Change in print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld{
   public static void main(String []args){
     if(args.length!=0) {
-			System.out.println("Hello "+args[0]+"....,\nLet's Code on JAVA");
+			System.out.println("Hello "+args[0]+"....,\nLet's start Code on JAVA");
 		}
 		else {
-			System.out.println("Hello just Code on JAVA");
+			System.out.println("Hello Bla-Bla. Let's begin Coding in JAVA");
 		}
   }
 }
